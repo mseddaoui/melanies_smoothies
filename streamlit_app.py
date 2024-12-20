a9332ce93746891045a8bb6a68fdfd7e45ceed28
Melissa Seddaoui <72555437+mseddaoui@users.noreply.github.com>
2024-12-20 10:17:09 +0100

    Update streamlit_app.py
    build and send REST API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
@@ -49,7 +50,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon ="✅")
-   
+
+# New section to display smoothifroot nutrition information
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response) 
     
     
 
