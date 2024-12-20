ec8fa06c1bfea5d992407e9353d3cee4cfcc6a7d
Melissa Seddaoui <72555437+mseddaoui@users.noreply.github.com>
2024-12-20 10:19:15 +0100

    Update streamlit_app.py
    expose the JSON data inside the Response Object

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if ingredients_list:
 
 # New section to display smoothifroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response) 
+st.text(smoothiefroot_response.json()) 
     
     
 
