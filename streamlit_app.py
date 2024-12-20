7b6f8e7f84670e4e1273124f5a4f20864d68cfd1
Melissa Seddaoui <72555437+mseddaoui@users.noreply.github.com>
2024-12-20 11:11:45 +0100

    Update streamlit_app.py
    A Strange-Looking Statement That Will Get Us the "Search On" Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 
 #convert the Snowpark Dataframe to a Pandas dataframe so wer can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 #Add a Multiselect
 ingredients_list = st.multiselect('Choose up 5 ingredients: ', 
@@ -44,6 +44,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosenn 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         # New section to display smoothifroot nutrition information
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
